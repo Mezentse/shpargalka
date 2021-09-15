5387052319f1129bf9dce378f52bef38e223a63f
Mezentse <90177766+Mezentse@users.noreply.github.com>
2021-09-15 13:15:06 +0300

    Add files via upload

---
+++ Source1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	double c = 1;
+	long h = 2;
+	char q = 8;
+	short w = 6;
+	unsigned char e = 0;
+	unsigned  short r = 0;
+	unsigned long t = 0;
+	unsigned int u = 10;
+	float i = 0;
+	long double o = 0;
+
+	int a1 ;
+	double c1 ;
+	long h1 ;
+	char q1 ;
+	short w1 ;
+	unsigned char e1 ;
+	unsigned  short r1;
+	unsigned long t1;
+	unsigned int u1 ;
+	float i1 ;
+	long double o1;
+
+	a1 = 3;
+	c1 = 4;
+	h1 = 5;
+	q1 = 9;
+	w1 = 7;
+	e1 = 0;
+	r1 = 0;
+	t1 = 0;
+	i1 = 0;
+	u1 = 11;
+	o1 = 0;
+
+
+	
+	printf("%d\n%lf\n%d\n%d\n%lf\n%d\n%hd\n%hd\n%d\n%d\n%u\n%u\n", a, c, h, a1, c1, h1, w, w1,q,q1,u,u1);
+
+	return 0;
+}
